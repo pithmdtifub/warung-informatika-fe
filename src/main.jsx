003ebdf7e308c43278e16bfe2a5c1ba781955b21
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2024-11-23 22:40:38 +0700

    chore: call app, BagianLogin, and BagianDashboard component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import Navbar from './components/NavbarComponent/Navbar.jsx'
-import BagianAtas from './components/DaftarMenu/BagianAtas.jsx'
-import BagianBawah from './components/DaftarMenu/BagianBawah.jsx'
+import App from './App.jsx'
+import BagianLogin from './components/Login/HalamanLogin.jsx'
+import BagianDashboard from './BagianDashboard.jsx'
+
+// import BagianAtas from './components/DashboardAdmin/BagianAtas.jsx'
+// import BagianBawah from './components/DashboardAdmin/BagianBawah.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <BagianLogin/>,
+  },
+  {
+    path: "/dashboard-admin",
+    element: <BagianDashboard />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <BagianAtas />
-    <BagianBawah />
+    <RouterProvider router={router} />
+    {/* <App/> */}
+    {/* <BagianAtas />
+    <BagianBawah /> */}
   </StrictMode>
 )
