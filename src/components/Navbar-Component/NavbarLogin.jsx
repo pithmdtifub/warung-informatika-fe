62b62c1188bcadb2f3cd8d50f3e8a676084ff49d
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2024-11-26 09:13:32 +0700

    feat: NavbarLogout for Dashboard Admin page

--- src/components/Navbar-Component/NavbarLogin.jsx
+++ src/components/Navbar-Component/NavbarLogin.jsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import { Container, Navbar, Button } from 'react-bootstrap';
 import './Navbar.css'
 
 
@@ -10,7 +8,7 @@ function navbarWarungInformatika() {
   return (
     <Navbar className="navbar sticky-top" expand="lg" bg="light" data-bs-theme="light">
       <Container fluid className="d-flex">
-          <Navbar.Brand className="navbar-brand">Warung Informatika</Navbar.Brand>
+          <Navbar.Brand className="navbar-brand-login">Warung Informatika</Navbar.Brand>
           <Link to={'/login'}>
             <Button className="button-login">Login</Button>
           </Link>
