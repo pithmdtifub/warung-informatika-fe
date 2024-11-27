4da0c75e449bcdfeda31d97f27f9680a68a9934c
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2024-11-27 22:55:58 +0700

    feat: The login page for username and password is working

--- src/components/Login/HalamanLogin.jsx
+++ src/components/Login/HalamanLogin.jsx
@@ -1,10 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container, Row, Form, Button, Card } from 'react-bootstrap';
-import { Link } from "react-router-dom";
-import { User } from 'lucide-react';
-import { Eye } from 'lucide-react';
+import { User, EyeClosed, Eye } from 'lucide-react';
 import './HalamanLogin.css'
 
 function BagianLogin() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/dashboard-admin")
+    }
+  });
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleLogin = async (e) => {
+      e.preventDefault(); 
+      setErrorMessage(""); 
+  
+      try {
+        const response = await fetch("https://9840-2407-0-3002-104e-598d-7e49-8393-eb00.ngrok-free.app/api/v1/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }), 
+        });
+  
+        const data = await response.json();
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+  
+          navigate("/dashboard-admin");
+        } else {
+          const err = data.error || data.errors;
+          if (typeof err == 'object') {
+            for (const [key, value] of Object.entries(err)) {
+              setErrorMessage(value);
+            }
+          } else {
+            setErrorMessage(err); 
+          }
+        }
+      } catch (error) {
+      }
+    };
+
     return (
     <>
     <p className='judul'>Warung Informatika</p>
@@ -16,19 +61,20 @@ function BagianLogin() {
                 <Card.Body>
                     <Card.Title className="login">Login</Card.Title>
                 </Card.Body>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3">
                     <div className="input-icon-wrapper">
-                        <Form.Control className="input-usn-pw" type="username" placeholder="Username" required/>
+                        <Form.Control className="input-usn-pw" type="username" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
                         <User className="icon-inside-input-halaman-login" />
                     </div>
                     <div className="input-icon-wrapper">
-                        <Form.Control className="input-usn-pw" type="password" placeholder="Password" required/>
-                        <Eye className="icon-inside-input-halaman-login" />
-                    </div>
-                    <Link to={'/dashboard-admin'}>
+                        <Form.Control className="input-usn-pw" type={showPassword ? "text" : "password"} placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        {showPassword ? (<EyeClosed onClick={() => setShowPassword(false)} style={{ cursor: "pointer" }}/>
+                        ) : (
+                        <Eye className="icon-inside-input-halaman-login" onClick={() => setShowPassword(true)} style={{ cursor: "pointer" }}/>)}
+                    </div>            
                         <Button type="submit" className="button-submit">Submit</Button>
-                    </Link>
+                        {errorMessage && (<small className="text-error mt-2 d-block text-center">{errorMessage}</small>)}
                 </Form.Group>
             </Form>
             </Card>
